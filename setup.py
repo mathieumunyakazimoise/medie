840f90c1520e4b5a4d9f18b0d24fd5746e408ebe
mathieumunyakazimoise <m.munyakazi1@alustudent>
2023-10-17 20:53:47 -0700

    medie change

---
+++ setup.py
@@ -0,0 +1,34 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="git-autopush",
+    version="0.2.5",
+    author="mathieu",
+    author_email="m.munyakazi1@alustudent.com",
+    description="Automates git functions: git add, git commit, and git push",
+    license="MIT",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/mathieumunyakazimoise/git-autopush",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Development Status :: 5 - Production/Stable",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+    ],
+    python_requires=">=3.6",
+    entry_points={
+        "console_scripts": [
+            "git-autopush = git_autopush.autopush:monitor_directory",
+        ],
+    },
+)
